2037ae077d69acaa81b90cefda67689e86a5c8df
mehrdad <javadabbasi72@gmail.com>
2020-08-12 01:05:21 +0430

    add lang

---
+++ src/Resources/Lang/en/panel.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'basic_information' => 'basic information',
+    'menu_information'  => 'menu information',
+    'menu'              => 'menu',
+    'menus'             => 'menus',
+    'new_menu'          => 'new menu',
+    'id'                => 'id',
+    'name'              => 'name',
+    'action'            => 'action',
+    'yes'               => 'yes',
+    'no'                => 'no',
+    'is_active'         => 'is active',
+    'category'          => 'category',
+    'page'              => 'page',
+    'url'               => 'url',
+    'system'             => 'system',
+    'pages'             => 'pages',
+
+
+];
